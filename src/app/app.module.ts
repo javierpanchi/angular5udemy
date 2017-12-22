cc9db8d10c884efaf781d7f198cbc8f3846d41ef
Javier S. Panchi <javier.s.panchi@au.abb.com>
2017-12-22 12:07:00 +1000

    Lesson18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+import { MaterialModule } from './extramodules/material/material.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
@@ -14,7 +18,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
